d42da8fce06f6baebc40753c3095f3d85586424a
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2017-10-29 15:45:57 -0400

    15:45

--- prueba2Java/src/java/controlador/MotoDAO.java
+++ prueba2Java/src/java/controlador/MotoDAO.java
@@ -54,22 +54,15 @@ public class MotoDAO implements GeneralDAOMoto{
             {
                 rut_persona = results.getInt("rut_persona");                                
                                 patente = results.getString("patente");
-                                cantPuertas = results.getInt("cant_puertas");
-                                cantAsientos = results.getInt("asientos");
-                                tipoAuto = results.getString("tipo_auto");
-                                cantAirbags = results.getInt("canti_aitbag");
-                                cambiosAutomaticos = results.getString("camb_automatico");
-                                electrico = results.getString("electrico");
-                                direccionAsistida = results.getString("dire_asistid");
-                                portaEquipaje = results.getString("port_equip");
                                 marca = results.getString("marca");
                                 rut_persona = results.getInt("rut_persona");
                                 foto= results.getString("foto");
                                 anyo= results.getInt("anyo");
                                 kilometraje = results.getInt("kilometraje");
                                 tipo_bencina = results.getString("tip_bencina");
-                                
-                                obj = new Moto(cantPuertas, cantAsientos, tipoAuto, cantAirbags, cambiosAutomaticos, electrico, direccionAsistida, portaEquipaje, patente, marca, anyo, foto, anyo, kilometraje, tipo_bencina);
+                                tipoAuto = results.getString("tipo_moto");
+   
+                                obj = new Moto(patente, marca, anyo, foto,kilometraje, tipo_bencina,tipo_moto);
                                 arrayMotos.add(obj);
                                 break;                                
             }
@@ -90,17 +83,79 @@ public class MotoDAO implements GeneralDAOMoto{
     
     @Override
     public Moto buscarDatos(String patente) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Moto obj =null;
+          try {
+
+                      Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+                        Connection connection = DriverManager.getConnection
+                                               ("jdbc:mysql://localhost:3306/empresa","root","");
+
+
+                        Statement statement = connection.createStatement();
+
+                        String query = "SELECT * from moto where patente='"+patente+"';" ;
+
+
+                         ResultSet results = statement.executeQuery(query);
+                        
+                        String campo1; 
+                        int campo2;                     
+                        String campo3;
+                        String campo4;
+                        int campo5;
+                        int campo6;
+                        String campo7;  
+                             
+                            while (results.next())
+                            {
+                                campo1 = results.getString("patente");
+                                campo2=results.getInt("rut_persona");
+                                campo3=results.getString("marca");
+                                campo4=results.getString("foto");
+                                campo5=results.getInt("anyo");
+                                campo6=results.getInt("kilometraje");
+                                campo7=results.getString("tipo_moto");
+                                
+                                
+                                if(campo1.equals(patente)){
+                                     obj=new Moto(campo1,campo2,campo3,campo4,campo5,campo6,campo7);
+                                     break;
+                                }
+                            }
+                  connection.close();
+         }
+         catch(java.lang.Exception ex){
+            System.out.println("Error: " + ex);
+         }
+ 
+         return obj;
     }
 
     @Override
     public int agregarDatosVehiculo(Moto moto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();            
+            Connection connection = DriverManager.getConnection
+                          ("jdbc:mysql://localhost:3306/empresa","root","");
+        
+            Statement statement = connection.createStatement();
+                          
+           String  agregarSQL = "INSERT INTO vehiculos (marca,rut_persona,foto,anyo,kilometraje,tip_bencina)"+
+                                 " VALUES('"+moto.getMarca()+"',"+moto.getRut()+",'"+moto.getFoto()+"',"+moto.getAnyo()+","+moto.getKilometraje()+",'"+moto.getTipo_bencina()+"')";
+            
+        }
+        catch(java.lang.Exception ex)
+        {
+            System.out.println("Error: " + ex);
+            return 0;
+        }
     }
 
     @Override
     public int agregarDatosMoto(Moto moto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
